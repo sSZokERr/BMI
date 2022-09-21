e30a6068e5d12712190edbc9e53b3227ecbaea36
szoker <MY_NAME@example.com>
2022-09-21 09:05:14 +0200

    main.js correct

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function bmi() {
 	let egyikszam = document.getElementById('meret').value;
 	let masikszam = document.getElementById('kg').value;
-	document.getElementById('ered').textContent = parseFloat(masikszam) / (parseFloat(egyikszam / 100) * (parseFloat(egyikszam / 100)));
+    let a = (parseFloat(masikszam) / (parseFloat(egyikszam / 100) * (parseFloat(egyikszam / 100))));
+    let result = a.toFixed(2)
+	document.getElementById('ered').textContent = result;
 }
 
 document.getElementById('szamit').addEventListener('click', bmi)
